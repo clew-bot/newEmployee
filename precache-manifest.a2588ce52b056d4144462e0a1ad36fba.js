bfc450f51648d565feebfbc3cb26f4b2093dc7db
Chad Lew <lewchad11@gmail.com>
2021-02-04 22:04:55 -0800

    Updates

---
+++ precache-manifest.a2588ce52b056d4144462e0a1ad36fba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de9e89c6837a2029d2bd996462ce11b",
+    "url": "/newBudget/index.html"
+  },
+  {
+    "revision": "a9c7958591549873c856",
+    "url": "/newBudget/static/css/main.2f23ba9b.chunk.css"
+  },
+  {
+    "revision": "1978153403f9577ffed7",
+    "url": "/newBudget/static/js/2.45fad9b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newBudget/static/js/2.45fad9b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9c7958591549873c856",
+    "url": "/newBudget/static/js/main.bab54e83.chunk.js"
+  },
+  {
+    "revision": "b346950881ae7aec1bdd",
+    "url": "/newBudget/static/js/runtime-main.b2f5f19c.js"
+  }
+]);
