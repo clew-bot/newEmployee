b1c09e35d3524617a8b5100b62c61ba5f04df567
Chad Lew <lewchad11@gmail.com>
2021-02-04 22:15:41 -0800

    fix pages

---
+++ build/precache-manifest.22f399398a22330c0cd920517733faa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc24d5870132438c92493bf7d7d1bec9",
+    "url": "/newEmployee/index.html"
+  },
+  {
+    "revision": "3b134a2cc5030f4c90ef",
+    "url": "/newEmployee/static/css/main.2f23ba9b.chunk.css"
+  },
+  {
+    "revision": "1978153403f9577ffed7",
+    "url": "/newEmployee/static/js/2.45fad9b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newEmployee/static/js/2.45fad9b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b134a2cc5030f4c90ef",
+    "url": "/newEmployee/static/js/main.763a7eb4.chunk.js"
+  },
+  {
+    "revision": "f58687d23654776bf8cf",
+    "url": "/newEmployee/static/js/runtime-main.3eedfde6.js"
+  }
+]);
